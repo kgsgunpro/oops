55ead7f47de29caf24898741ed454229af7f6d63
kgsgun <kgsgunpro@gmail.com>
2026-05-16 20:45:03 +0530

    basic structure of project is ready

---
+++ oops_proj.py
@@ -0,0 +1,28 @@
+#facebook like project
+class Chatbook:
+    def __init__(self):
+        self.username = ''
+        self.password = ''
+        self.loggedin = False
+        self.menu()
+    
+    def menu(self):
+        user_input=input('welcome to chatbook \n' \
+        '1.press 1 to signup \n' \
+        '2. press 2 to signin \n' \
+        '3. press 3 to write a post \n' \
+        '4. press 4 to message a friend \n' \
+        '5. press any other key to exit \n')
+        if user_input == '1' :
+            pass
+        elif user_input == '2' :
+            pass
+        elif user_input == '3' :
+            pass
+        elif user_input == '4' :
+            pass
+        else :
+            exit()
+obj = Chatbook()
+
+
