a469e4e6a1c873ee9da876b07d3ba3ef1c13a06f
kgsgun <kgsgunpro@gmail.com>
2026-05-16 19:21:52 +0530

    added method travel() in oops1.py in employee class

--- oops1.py
+++ oops1.py
@@ -6,7 +6,8 @@ class Employee:
         self.salary= 40000
         self.designation = 'sde'
 # creating obj/instance of class
-    
+    def travel(self,destination):
+        print(f'employee is now traveling to { destination}')
 ravi = Employee()
-print(ravi.salary)
+print(ravi.salary , ravi.travel('vizag'))
 
