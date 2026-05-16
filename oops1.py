9c378910c09d2fbda7d6e35adda4da0a72b64898
kgsgun <kgsgunpro@gmail.com>
2026-05-16 19:28:54 +0530

    commiting

--- oops1.py
+++ oops1.py
@@ -2,11 +2,14 @@
 class Employee:
     # constructor
     def __init__(self):
+        print('started executing attributes/data')
         self.id =123
         self.salary= 40000
         self.designation = 'sde'
+        print('started executing attributes/data')
 # creating obj/instance of class
     def travel(self,destination):
+        print('travel func called manually')
         print(f'employee is now traveling to { destination}')
 ravi = Employee()
 print(ravi.salary , ravi.travel('vizag'))
