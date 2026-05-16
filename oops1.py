04aafd29224df05ba9fd47545e388329d9612045
kgsgun <kgsgunpro@gmail.com>
2026-05-16 19:11:37 +0530

    creating a class Employee inside oops1.py and creating content.txt

---
+++ oops1.py
@@ -0,0 +1,12 @@
+#iniating a class 
+class Employee:
+    # constructor
+    def __init__(self):
+        self.id =123
+        self.salary= 40000
+        self.designation = 'sde'
+# creating obj/instance of class
+    
+ravi = Employee()
+print(ravi.salary)
+
