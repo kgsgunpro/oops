f866aa56d5d280e6672310b7af7b8dc39f30e43b
kgsgun <kgsgunpro@gmail.com>
2026-05-28 18:14:26 +0530

    signup and signin added

--- oops_proj.py
+++ oops_proj.py
@@ -14,15 +14,44 @@ class Chatbook:
         '4. press 4 to message a friend \n' \
         '5. press any other key to exit \n')
         if user_input == '1' :
-            pass
+            self.signup()
         elif user_input == '2' :
-            pass
+            self.signin()
         elif user_input == '3' :
             pass
         elif user_input == '4' :
             pass
         else :
             exit()
+    
+    def signup(self):
+        email = input('enter your email : ')
+        passw = input('enter your password')
+        self.username= email
+        self.password = passw
+        print('You have signed up successfully! \n ')
+        self.menu()
+    
+    def signin(self):
+        if self.username =='' and self.password=='' :
+            print('please sign up')
+        else:
+            email = input('enter your email : ')
+            passw = input('enter your password')
+            if self.username==email and self.password ==passw :
+                print('successfully logged in')
+                self.loggedin =True
+            else:
+                print('incorrect credentials')
+        print('\n')        
+        self.menu()
+
+
+
+
+
+
+        
 obj = Chatbook()
 
 
